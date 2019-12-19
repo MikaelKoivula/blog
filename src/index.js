f3569238785452e8dc977bc8040230639ab32f8d
Mikael Koivula <mikaelkoivula@Mikaels-MacBook-Pro.local>
2019-12-19 12:14:30 +0200

    new action fetchpostsandusers & cleaning  code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+
 const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
